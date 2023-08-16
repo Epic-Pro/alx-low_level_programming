20ca87d7d545c9dd4cc7e4705e0b69b5408534d1
Epic-Pro <moriseh@gmail.com>
2023-08-16 13:37:20 -0700

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,11 +16,10 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-		else if (a > &&c > a)
-		{
-			largest = c;
-		}
-
+	else if (a > b && c > a)
+	{
+		largest = c;
+	}
 	else if (b > c)
 	{
 		largest = b;
