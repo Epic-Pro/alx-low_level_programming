17f12b66a4ff29a9a4b538e7edff71a074d2fc13
Epic-Pro <moriseh@gmail.com>
2023-08-21 07:25:13 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -2,11 +2,7 @@
 
 /**
  * _putchar - writes character c to stdout
-<<<<<<< HEAD
- * @c: char to be printed
-=======
  * @c: chat to be printed
->>>>>>> ebf262974f34308f9c901e3c786fcabcbd260db6
  * Return: On success
  */
 int _putchar(char c)
