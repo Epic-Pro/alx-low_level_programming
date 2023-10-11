7ba83e3fa7b3f421f5456d24cb38097d02516a9d
Epic-Pro <morrismulela@gmail.com>
2023-10-11 03:25:27 -0700

    3-cp.c

---
+++ 0x15-file_io/error_functions.h
@@ -0,0 +1,8 @@
+#ifndef Error_Functions_H
+#define Error_Functions_H
+
+void error_98(int p1, const char *buffer, const char *filename);
+void error_99(int p0,const char *buffer, const char *filename);
+void error_100(int fd, const char *buffer);
+
+#endif /* Error_Functions_H */
